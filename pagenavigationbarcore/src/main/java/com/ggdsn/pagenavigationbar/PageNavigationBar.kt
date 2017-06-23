54d6f378414cd645fd0e3c26285f3a710104c3e9
LiaoXingyu <gdannyliao@gmail.com>
2017-06-23 14:32:50 +0800

    将继承textview，改成viewGroup实现

--- pagenavigationbarcore/src/main/java/com/ggdsn/pagenavigationbar/PageNavigationBar.kt
+++ pagenavigationbarcore/src/main/java/com/ggdsn/pagenavigationbar/PageNavigationBar.kt
@@ -7,57 +7,54 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.os.Build
+import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
 import android.util.DisplayMetrics
 import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.Scroller
 
 /**
  * Created by LiaoXingyu on 21/05/2017.
  */
 
-class PageNavigationBar : AppCompatTextView {
+class PageNavigationBar @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ViewGroup(context, attrs, defStyleAttr) {
     companion object {
         private const val STATE_INIT = 0
         private const val STATE_MIDDLE = 1
     }
 
-    private var forwardArrow: Drawable? = null
-    private var displayMetrics: DisplayMetrics? = null
-    private var backArrow: Drawable? = null
+    private val textView = AppCompatTextView(context)
+    private val backArrowView = AppCompatImageView(context)
+    private val forwardArrowView = AppCompatImageView(context)
+
+    private val forwardArrow: Drawable
+    private val displayMetrics: DisplayMetrics
+    private val backArrow: Drawable
     private val minPaddingDp = 8
     private val steps: MutableList<String> = mutableListOf()
     private var currentStepIdx = 0
     private var state = STATE_INIT
-    private var lineScroller: Scroller
+    private val lineScroller = Scroller(context)
     private val paint = Paint()
     private val lineRect = Rect()
     private val lineWidthDp = 4
     private var lineWidth: Int = 0
+    private val marginDp = 10
+    /**
+     * 用于标记那条线偏移到的位置
+     */
     private var linex: Int = 0
 
-    constructor(context: Context) : super(context) {
-        init(context, null, 0)
-        lineScroller = Scroller(context)
-    }
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(context, attrs, android.R.attr.textViewStyle)
-        lineScroller = Scroller(context)
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init(context, attrs, defStyleAttr)
-        lineScroller = Scroller(context)
-    }
-
-    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+    init {
         val res = context.resources
         displayMetrics = res.displayMetrics
-        lineWidth = (displayMetrics!!.density * lineWidthDp).toInt()
+        lineWidth = (displayMetrics.density * lineWidthDp).toInt()
+        paint.color = Color.WHITE
 
-        gravity = Gravity.CENTER
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             forwardArrow = context.getDrawable(R.drawable.selector_pagenavigationbar_forward_arrow)
             backArrow = context.getDrawable(R.drawable.ic_arrow_back_black_24dp)
@@ -66,30 +63,74 @@ class PageNavigationBar : AppCompatTextView {
             backArrow = res.getDrawable(R.drawable.ic_arrow_back_black_24dp)
         }
 
+        backArrowView.setImageResource(R.drawable.ic_arrow_back_black_24dp)
         setBackgroundResource(R.drawable.shape_page_navigation_bar_background)
-        setCompoundDrawablesWithIntrinsicBounds(null, null, forwardArrow, null)
+
+        backArrowView.setImageResource(R.drawable.ic_arrow_back_black_24dp)
+        forwardArrowView.setImageResource(R.drawable.selector_pagenavigationbar_forward_arrow)
+
+        textView.gravity = Gravity.CENTER
 
         if (isInEditMode) {
-            text = "选择订单"
+            textView.text = "选择订单"
         } else {
-            setTextColor(res.getColorStateList(R.color.selector_pagenavigationbar_text))
+            textView.setTextColor(res.getColorStateList(R.color.selector_pagenavigationbar_text))
+
+            val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.PageNavigationBar, defStyleAttr, 0)
+            textView.text = typedArray.getString(R.styleable.PageNavigationBar_text)
+            typedArray.recycle()
         }
-        paint.color = Color.WHITE
+
+        addView(backArrowView)
+        addView(textView)
+        addView(forwardArrowView)
     }
 
+    private val maxHeightDp: Int = 64
+
+    private var maxTextWidth: Int? = 0
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        println("onMeasure")
-        val minPadding = (minPaddingDp * displayMetrics!!.density).toInt()
+
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        var height = MeasureSpec.getSize(heightMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val density = displayMetrics.density
+
+        if (heightMode == MeasureSpec.AT_MOST) {
+            val maxHeight = (density * maxHeightDp - 2 * density * marginDp).toInt()
+            height = if (height > maxHeight) maxHeight else height
+        }
+        setMeasuredDimension(width, height)
+        linex = measuredWidth / 4
+
+        val minPadding = (minPaddingDp * density).toInt()
         var paddingTop = paddingTop
         if (paddingTop < minPadding) paddingTop = minPadding
         var paddingBottom = paddingBottom
         if (paddingBottom < minPadding) paddingBottom = minPadding
 
-        //因为右边有图案而左边没有，要修正标题栏的位置，让其居中。
-        val bounds = forwardArrow!!.bounds
-        setPadding(minPadding + bounds.right - bounds.left, paddingTop, minPadding, paddingBottom)
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        linex = measuredWidth / 4
+        val contentHeight = height - minPadding * 2
+        val iconWidthSpec = MeasureSpec.makeMeasureSpec(contentHeight, MeasureSpec.EXACTLY)
+        val iconHeightSpec = MeasureSpec.makeMeasureSpec(contentHeight, MeasureSpec.EXACTLY)
+
+        backArrowView.measure(iconWidthSpec, iconHeightSpec)
+        forwardArrowView.measure(iconWidthSpec, iconHeightSpec)
+
+        backArrowView.translationX = -linex.toFloat()
+//        因为右边有图案而左边没有，要修正标题栏的位置，让其居中。
+        setPadding(minPadding, paddingTop, minPadding, paddingBottom)
+
+        maxTextWidth = width - paddingLeft - paddingRight - contentHeight
+        textView.measure(MeasureSpec.makeMeasureSpec(maxTextWidth!!, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(contentHeight, MeasureSpec.AT_MOST))
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        val width = r - l
+        val height = b - t
+        backArrowView.layout(paddingLeft, paddingTop, paddingLeft + backArrowView.measuredWidth, height - paddingBottom)
+        textView.layout(paddingLeft, paddingTop, paddingLeft + textView.measuredWidth, height - paddingBottom)
+        forwardArrowView.layout(width - paddingRight - forwardArrowView.measuredWidth, paddingTop, width - paddingRight, height - paddingBottom)
     }
 
     fun setTitles(titles: List<String>) {
@@ -116,12 +157,13 @@ class PageNavigationBar : AppCompatTextView {
         }
         currentStepIdx = step
         val s = steps[step]
-        text = s
+        textView.text = s
         when (step) {
-            0 -> showState(STATE_INIT)
+            0 -> {
+                if (state != STATE_INIT) showState(STATE_INIT)
+            }
             else -> {
-                if (state != STATE_MIDDLE)
-                    showState(STATE_MIDDLE)
+                if (state != STATE_MIDDLE) showState(STATE_MIDDLE)
             }
         }
     }
@@ -146,8 +188,11 @@ class PageNavigationBar : AppCompatTextView {
 
     override fun computeScroll() {
         if (lineScroller.computeScrollOffset()) {
-            val left = lineScroller.currX - lineWidth
-            lineRect.set(if (left < 0) 0 else left, 0, lineScroller.currX, height)
+            var left = lineScroller.currX - lineWidth
+            left = if (left < 0) 0 else left
+            textView.translationX = left.toFloat()
+            backArrowView.translationX = left.toFloat() - linex
+            lineRect.set(left, 0, lineScroller.currX, height)
             invalidate()
         }
     }
